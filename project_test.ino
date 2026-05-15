7d0055f3929a14399d7824b8c7d5e84162da29e0
zxcds-qwer <dkdlelsjqtek@gmail.com>
2026-05-15 13:44:05 +0900

    FSR 2, difference code

--- project_test.ino
+++ project_test.ino
@@ -1,16 +1,21 @@
 /*
-  FSR 406 압력센서 2개 테스트
+  FSR 406 압력센서 2개
+  좌우 압력 편향 감지 코드
   Arduino UNO R3
 */
 
-const int fsrPin1 = A0;
-const int fsrPin2 = A1;
+const int leftPin = A0;
+const int rightPin = A1;
 
-int fsrValue1 = 0;
-int fsrValue2 = 0;
+int leftValue = 0;
+int rightValue = 0;
 
-float weight1 = 0;
-float weight2 = 0;
+float LEFT = 0.0;
+float RIGHT = 0.0;
+
+float difference = 0.0;
+
+String direction = "";
 
 void setup() {
 
@@ -21,29 +26,56 @@ void setup() {
 void loop() {
 
   // 센서값 읽기
-  fsrValue1 = analogRead(fsrPin1);
-  fsrValue2 = analogRead(fsrPin2);
+  leftValue = analogRead(leftPin);
+  rightValue = analogRead(rightPin);
 
   // 임시 kg 변환
-  weight1 = (fsrValue1 / 1023.0) * 10.0;
-  weight2 = (fsrValue2 / 1023.0) * 10.0;
+  LEFT = (leftValue / 1023.0) * 10.0;
+  RIGHT = (rightValue / 1023.0) * 10.0;
+
+  // 차이값 계산
+  // 양수면 LEFT 압력이 더 큼
+  // 음수면 RIGHT 압력이 더 큼
+  difference = LEFT - RIGHT;
+
+  // 방향 판단
+  if (difference > 0.5) {
+
+    direction = "LEFT";
+
+  }
+  else if (difference < -0.5) {
+
+    direction = "RIGHT";
+
+  }
+  else {
+
+    direction = "CENTER";
+
+  }
 
   // 출력
-  Serial.print("Sensor1 : ");
-  Serial.print(fsrValue1);
-
-  Serial.print("   Weight1 : ");
-  Serial.print(weight1);
+  Serial.print("LEFT : ");
+  Serial.print(LEFT, 2);
   Serial.print(" kg");
 
   Serial.print("    |    ");
 
-  Serial.print("Sensor2 : ");
-  Serial.print(fsrValue2);
+  Serial.print("RIGHT : ");
+  Serial.print(RIGHT, 2);
+  Serial.print(" kg");
 
-  Serial.print("   Weight2 : ");
-  Serial.print(weight2);
-  Serial.println(" kg");
+  Serial.print("    |    ");
+
+  Serial.print("DIFF : ");
+  Serial.print(difference, 2);
+  Serial.print(" kg");
+
+  Serial.print("    |    ");
+
+  Serial.print("BIAS : ");
+  Serial.println(direction);
 
   delay(1000);
 }
